691bce2af1a22531bf4287fea7d48c7edf7399ce
alma-hernandez <1984ahern@gmail.com>
2024-02-12 15:45:36 -0600

    trying post request

--- app/javascript/src/requests.js
+++ app/javascript/src/requests.js
@@ -1,21 +1,6 @@
 import $ from 'jquery';
 
-var indexTasks = function () {
-  var request = {
-    type: 'GET',
-    url: 'api/tasks?api_key=1',
-    success: function (response) {
-      console.log(response);
-    },
-    error: function (request, errorMsg) {
-      console.log(request, errorMsg);
-    }
-  }
 
-  $.ajax(request);
-};
-
-indexTasks();
 
 var postTask = function (content) {
     var request = {
