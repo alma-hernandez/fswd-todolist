77f93a4d900f60a0e59debc55995e261e63dc9b5
alma-hernandez <1984ahern@gmail.com>
2024-02-12 15:27:24 -0600

    with jquery installed

--- app/javascript/src/requests.js
+++ app/javascript/src/requests.js
@@ -1,1 +1,38 @@
-console.log('ajax requests');
+import $ from 'jquery';
+
+var indexTasks = function () {
+  var request = {
+    type: 'GET',
+    url: 'api/tasks?api_key=1',
+    success: function (response) {
+      console.log(response);
+    },
+    error: function (request, errorMsg) {
+      console.log(request, errorMsg);
+    }
+  }
+
+  $.ajax(request);
+};
+
+indexTasks();
+
+var postTask = function (content) {
+    var request = {
+      type: 'POST',
+      url: 'api/tasks?api_key=1',
+      data: {
+        task: {
+          content: content
+        }
+      },
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (request, errorMsg) {
+        console.log(request, errorMsg);
+      }
+    }
+    $.ajax(request);
+  };
+  
