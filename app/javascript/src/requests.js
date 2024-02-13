99d08097448bd9747e101ad03986b89edb041d2c
alma-hernandez <1984ahern@gmail.com>
2024-02-13 14:05:34 -0600

    added functions for routes provided

--- app/javascript/src/requests.js
+++ app/javascript/src/requests.js
@@ -32,3 +32,59 @@ export var postTask = function (content, successCB, errorCB) {
 
   $.ajax(request);
 };
+
+export var showTaskId = function (taskId, successCB, errorCB) {
+  var request = {
+    type: 'GET',
+    url: 'api/tasks/' + taskId + '?api_key=1', // Assuming you need to pass the task ID in the URL
+    success: successCB,
+    error: errorCB
+  }
+
+  $.ajax(request);
+};
+
+export var updateTaskId = function (taskId, successCB, errorCB) {
+  var request = {
+    type: 'UPDATE',
+    url: 'api/tasks/' + taskId + '?api_key=1',
+    success: successCB,
+    error: errorCB
+  }
+
+  $.ajax(request);
+};
+
+export var TaskMark_Complete = function (taskId, successCB, errorCB) {
+  var request = {
+    type: 'MARK_COMPLETE',
+    url: 'api/tasks/' + taskId + '?api_key=1',
+    success: successCB,
+    error: errorCB
+  }
+
+  $.ajax(request);
+};
+
+export var TaskActive= function (taskId, successCB, errorCB) {
+  var request = {
+    type: 'ACTIVE',
+    url: 'api/tasks/' + taskId + '?api_key=1',
+    success: successCB,
+    error: errorCB
+  }
+
+  $.ajax(request);
+};
+
+export var Destroy= function (taskId, successCB, errorCB) {
+  var request = {
+    type: 'DESTROY',
+    url: 'api/tasks/' + taskId + '?api_key=1',
+    success: successCB,
+    error: errorCB
+  }
+
+  $.ajax(request);
+};
+
