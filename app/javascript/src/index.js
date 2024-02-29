bb4af9507dba6918d485a7a9c77a134afd884660
alma-hernandez <1984ahern@gmail.com>
2024-02-29 09:37:28 -0600

    added task complete and active

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -29,4 +29,27 @@ indexTasks(function (response) {
     $("#tasks").html(htmlString);
 });
 
+TaskMark_Complete(function (response) {
+  var htmlString = response.tasks.map(function(task) {
+    return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
+      " + task.content + "\
+      </div>";
+  });
+
+  $("#tasks").html(htmlString);
 });
+
+
+
+TaskActive(function (resposne) {
+  var htmlString = response.tasks.map(function(task) {
+    return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
+      " + task.content + "\
+      </div>";
+  });
+
+  $("#tasks").html(htmlString);
+});
+});
+
+
